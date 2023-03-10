ecbffbe017a76eba1be84adf652ecfc91923cc5b
parasales <127375317+parasales@users.noreply.github.com>
2023-03-09 21:43:10 -0600

    Add files via upload

---
+++ tatisite/js/scripts.js
@@ -0,0 +1,8 @@
+/*!
+* Start Bootstrap - Coming Soon v6.0.6 (https://startbootstrap.com/theme/coming-soon)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-coming-soon/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+<script src="https://kit.fontawesome.com/53dff5c34e.js" crossorigin="anonymous"></script>
